d368e820ef4bfb26a74bd02379f8c784b4c92fde
Huckleberry Febbo <febbo@umich.edu>
2016-12-03 21:34:26 -0500

    main module

--- src/NLOptControl.jl
+++ src/NLOptControl.jl
@@ -1,5 +1,28 @@
 module NLOptControl
 
-# package code goes here
 
-end # module
+using Media, DifferentialEquations, Dierckx, Parameters, Interpolations
+
+# to copy a particular piece of code in some location
+macro def(name, definition)
+  return quote
+    macro $name()
+      esc($(Expr(:quote,definition)))
+    end
+  end
+end
+
+
+export
+  # Objects
+
+  # Functions
+
+  # Macros and Support Functions
+
+  # MAKE SURE YOU REMOVE THE FINAL COMMA!!
+
+@with_kw immutable Params @deftype Float64
+  # define model parameters
+
+end
