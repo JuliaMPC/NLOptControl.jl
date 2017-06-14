706154401d0c8427e175fb24346a63d5904b6db7
Huckleberry Febbo <febbo@umich.edu>
2017-06-14 10:43:47 -0400

    fixed test functions

--- test/ocp.jl
+++ test/ocp.jl
@@ -8,9 +8,21 @@ function BrysonDenham{T<:Any}(n::NLOpt,x::Array{T,2},u::Array{T,2})
   return dx
 end
 
-@testset "BrysonDenham with (:integrationScheme=>$(integrationConfig[1])) and (:integrationMethod=>$(integrationConfig[2]))" for integrationConfig in integrationConfigs
+@testset "BrysonDenham with (:integrationScheme=>$(integrationConfig[1])) and (:integrationMethod=>$(integrationConfig[2]) using function)" for integrationConfig in integrationConfigs
   L=1/6;
-  n=define!(;stateEquations=BrysonDenham,numStates=2,numControls=1,X0=[0.,1],XF=[0.,-1.],XL=[0.,NaN],XU=[L,NaN],CL=[NaN],CU=[NaN]);
+  n=define!(BrysonDenham;numStates=2,numControls=1,X0=[0.,1],XF=[0.,-1.],XL=[0.,NaN],XU=[L,NaN],CL=[NaN],CU=[NaN]);
+  configure!(n;(:integrationMethod=>integrationConfig[1]),(:integrationScheme=>integrationConfig[2]),(:finalTimeDV=>false),(:tf=>1.0));
+  obj=integrate!(n,n.r.u[:,1];C=0.5,(:variable=>:control),(:integrand=>:squared));
+  @NLobjective(n.mdl,Min,obj);
+  optimize!(n);
+  @test isapprox(4/(9*L),n.r.obj_val[1],atol=tol)
+end
+
+BrysonDenham_EXP=[:(x2[j]),:(u1[j])]
+
+@testset "BrysonDenham with (:integrationScheme=>$(integrationConfig[1])) and (:integrationMethod=>$(integrationConfig[2]) using expressions)" for integrationConfig in integrationConfigs
+  L=1/6;
+  n=define!(BrysonDenham_EXP;numStates=2,numControls=1,X0=[0.,1],XF=[0.,-1.],XL=[0.,NaN],XU=[L,NaN],CL=[NaN],CU=[NaN]);
   configure!(n;(:integrationMethod=>integrationConfig[1]),(:integrationScheme=>integrationConfig[2]),(:finalTimeDV=>false),(:tf=>1.0));
   obj=integrate!(n,n.r.u[:,1];C=0.5,(:variable=>:control),(:integrand=>:squared));
   @NLobjective(n.mdl,Min,obj);
