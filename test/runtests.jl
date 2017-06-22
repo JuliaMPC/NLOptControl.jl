bf28f680bbf4ac224c528bb6d4fbbe0a02dcf3b7
Huckleberry Febbo <febbo@umich.edu>
2017-06-22 17:51:12 -0400

    updated .git file and removed Ipopt add and build in travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,7 @@ const tol=1e-2;
 const integrationConfigs=[:lgrExplicit,:trapezoidal,:bkwEuler]
 
 include(Pkg.dir("NLOptControl/test/ocp.jl"))
+
+
+#using NBInclude
+#nbinclude(joinpath(dirname(@__FILE__), "..", "examples", "tutorial.ipynb"))
