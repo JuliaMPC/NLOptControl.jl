e8559f4c7cb26517ebaffb9af33b161b5b8c4ada
Huckleberry Febbo <febbo@umich.edu>
2018-03-11 14:11:19 -0400

    removed n.s.save in optimize

--- src/Base.jl
+++ src/Base.jl
@@ -546,14 +546,14 @@ Date Create: 2/6/2017, Last Modified: 5/29/2017 \n
 """
 function optimize!(n;Iter::Int64=0)
   t1=time(); status=JuMP.solve(n.mdl); t2=time();
-  if n.s.save
+#  if n.s.save
     n.r.status=status;
     n.r.t_solve=t2-t1;
     n.r.obj_val=getobjectivevalue(n.mdl);
     n.r.iter_nums=Iter;    # possible iteration number for a higher level algorithm
     n.r.eval_num=n.r.eval_num+1;
     postProcess!(n);      # temporarily save data
-  end
+#  end
   return nothing
 end
 
